1c89bc35f630e8601fe18ea738e9e0b7529b9ff1
ojulio-dev <juliocontact15@gmail.com>
2025-08-01 15:40:10 -0300

    Criando uma nota

--- App/Controllers/Notas/CriarController.php
+++ App/Controllers/Notas/CriarController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers\Notas;
 
+use Core\Database;
+use Core\Validacao;
+
 class CriarController 
 {
     public function index()
@@ -11,6 +14,37 @@ class CriarController
 
     public function store()
     {
-        dd($_POST);
+        $validacao = Validacao::validar([
+            'titulo' => ['required', 'min:3', 'max:255'],
+            'nota' => ['required']
+        ], $_POST);
+
+        if ($validacao->naoPassou()) {
+            return view('notas/criar');
+        }
+
+        $database = new Database(config('database'));
+
+        $database->query(
+            query: "insert into notas (usuario_id, titulo, nota, data_criacao, data_atualizacao)
+                values (
+                    :usuario_id,
+                    :titulo,
+                    :nota,
+                    :data_criacao,
+                    :data_atualizacao
+                )
+            ",
+            params: [
+                'usuario_id' => auth()->id,
+                'titulo' => $_POST['titulo'],
+                'nota' => $_POST['nota'],
+                'data_criacao' => date('Y-m-d H:i:s'),
+                'data_atualizacao' => date('Y-m-d H:i:s')
+            ]
+        );
+
+        flash()->push('mensagem', 'Nota Criada com sucesso!');
+        return redirect('/notas');
     }
 }
--- views/notas/criar.view.php
+++ views/notas/criar.view.php
@@ -1,3 +1,5 @@
+<?php $validacoes = flash()->get('validacoes'); ?>
+
 <div class="bg-base-300 rounded-l-box w-56">
     <div class="bg-base-200 p-4">+ Nova Nota</div>
 </div>
@@ -6,12 +8,18 @@
     <form action="/notas/criar" method="POST" class="flex flex-col space-y-6">
         <fieldset class="fieldset">
             <legend class="fieldset-legend">Título</legend>
-            <input type="text" class="input w-full" placeholder="Type here" />
+            <input type="text" name="titulo" class="input w-full" />
+            <?php if (isset($validacoes['titulo'])): ?>
+                <div class="mt-1 text-xs text-error"><?= $validacoes['titulo'][0] ?></div>
+            <?php endif; ?>
         </fieldset>
 
         <fieldset class="fieldset">
             <legend class="fieldset-legend">Sua nota</legend>
-            <textarea class="textarea h-24 w-full" placeholder="Bio"></textarea>
+            <textarea class="textarea h-24 w-full" name="nota"></textarea>
+            <?php if (isset($validacoes['nota'])): ?>
+                <div class="mt-1 text-xs text-error"><?= $validacoes['nota'][0] ?></div>
+            <?php endif; ?>
         </fieldset>
 
         <div class="flex justify-end items-center">
