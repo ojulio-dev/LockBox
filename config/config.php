33a921ce237f220673ef54525c0c13b9ae7268af
ojulio-dev <juliocontact15@gmail.com>
2025-08-06 14:20:25 -0300

    Funções de criptografia

--- config/config.php
+++ config/config.php
@@ -11,5 +11,9 @@ return [
         // 'dbname' => 'bookwise',
         // 'user' => 'root',
         // 'charset' => 'utf8mb4'
+    ],
+    'security' => [
+        'first_key' => '9mavforAPcS68mZ4yrUjZ8xSE16Ky+6kb6+VxyjA85w=',
+        'second_key' => '1QpWpDs7c2XoKT+m+hhKGkAgLD/d2P8Jh7YHj4XpeB67tJsBwOUdVROePkEsP3WkryFaxJtFaeEaWhPZN1P2fQ=='
     ]
 ];
